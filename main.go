1b27c9d2d2d8605a43dffd3da96f59901156e1e1
Liubomur Puliak <104902816+liubkkkko@users.noreply.github.com>
2022-06-15 09:45:55 +0300

    Update main.go
    1. Змінив конструкцію if на простішу
    2. Переніс оголошення константи поза тіло функції
    3. Витер лишні дужки

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+const myMoney float64 = 23
+
 func main() {
-	const myMoney float64 = 23
+
 	var (
 		applePrice   float64 = 5.99
 		pearPrice    float64 = 7
@@ -16,18 +18,15 @@ func main() {
 	appleCost := float64(amountApples) * applePrice
 	pearCost := float64(amountPears) * pearPrice
 	fruitCost := appleCost + pearCost
-	numbersPears := int((myMoney) / (pearPrice))
-	numbersApples := int((myMoney) / (applePrice))
+	numbersPears := int(myMoney / pearPrice)
+	numbersApples := int(myMoney / applePrice)
 	priceTwoApples := applePrice * 2
 	priceTwoPears := pearPrice * 2
 	priceFourFruits := priceTwoApples + priceTwoPears
-	if priceFourFruits <= myMoney {
-		solvency = true
-	}
+	solvency = priceFourFruits <= myMoney
 	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? --", fruitCost, "грн.")
 	fmt.Println("2. Скільки груш ми можемо купити? --", numbersPears, "шт.")
 	fmt.Println("3. Скільки яблук ми можемо купити? --", numbersApples, "шт.")
 	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука? --", solvency)
 
 }
-
